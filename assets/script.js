8e993a18fe397ca96b2c06755c86622c9d1b6122
Lulu Jovovich <lulujovovich@Lulus-MBP.lan>
2022-07-25 15:39:52 -0700

    bulk of work

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,123 @@
-// Assignment code here
-
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
+
+// Assignment code here
+ var special = [  '@','%','+','\\','/',"'",'!',
+ '#',
+ '$',
+ '^',
+ '?',
+ ':',
+ ',',
+ ')',
+ '(',
+ '}',
+ '{',
+ ']',
+ '[',
+ '~',
+ '-',
+ '_',
+ '.',]
+ var uppercase = [  'A',
+ 'B',
+ 'C',
+ 'D',
+ 'E',
+ 'F',
+ 'G',
+ 'H',
+ 'I',
+ 'J',
+ 'K',
+ 'L',
+ 'M',
+ 'N',
+ 'O',
+ 'P',
+ 'Q',
+ 'R',
+ 'S',
+ 'T',
+ 'U',
+ 'V',
+ 'W',
+ 'X',
+ 'Y',
+ 'Z',]
+var lowercase =  [ 'a',
+'b',
+'c',
+'d',
+'e',
+'f',
+'g',
+'h',
+'i',
+'j',
+'k',
+'l',
+'m',
+'n',
+'o',
+'p',
+'q',
+'r',
+'s',
+'t',
+'u',
+'v',
+'w',
+'x',
+'y',
+'z',
+]
+
+var number = "1234567890";
+
 // Write password to the #password input
-function ask1(){
+
+
+function generatePassword(){
     var length = prompt("Please choose a character length between 8 and 128.");
 
     if (length < 8 || length > 128){
         prompt("Refresh the page and enter a number between 8 and 128.");
-    } else if (length > 8 && length < 128){
-        prompt("Enter your desired character type: uppercase, lowercase, special, numeric.")
+    } 
+
+    var tempPass = '';
+    var password = '';
+
+    var specialChar = confirm("Do you want to include special characters?");
+
+    if (specialChar) {
+        tempPass += special;
     }
+
+    var numbCar = confirm("Do you want to include numbers?");
+
+    if (numbCar) {
+        tempPass += number;
+    }
+
+    for (var i= 0; i < length ; i++){
+        var index = Math.floor(Math.random() * tempPass.length);
+        password += tempPass[index];
+    }
+    return password;
 }
 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click",ask1)
+
 generateBtn.addEventListener("click", writePassword);
